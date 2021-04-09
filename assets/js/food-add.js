1ec5620dce82f5cc4d19af51c5d18800c910d56e
faithruth <faithruth27@gmail.com>
2021-04-09 14:32:24 +0300

    Added product variations to cart

--- assets/js/food-add.js
+++ assets/js/food-add.js
@@ -8,23 +8,21 @@ jQuery(document).ready(function($){
         let values = [];
         var product_id = $('input[type=hidden]').val();
         values = Array.from( document.querySelectorAll( 'input[type=checkbox]:checked' )).map(item=>item.value);
-            
-        values.forEach(value => {
-            $.ajax({
-                url: ajax_object.ajax_url,
-                data: {
-                    'action': 'food_ajax_add_to_cart',
-                    'product_id': product_id,
-                    'variation_id': value
-                },
-                type: 'post',
-                success: function(res){
-                    console.log('success');
-                },
-                error: function(err){
-                    console.log(err);
-                },
-            });
+        console.log(values);
+        $.ajax({
+            url: ajax_object.ajax_url,
+            data: {
+                'action': 'food_ajax_add_to_cart',
+                'product_id': product_id,
+                'variation': values
+            },
+            type: 'post',
+            success: function(res){
+                console.log('success');
+            },
+            error: function(err){
+                console.log(err);
+            },
         });
 
         //formSelected.reset();
